9292fac91c8c843e49b4979b915bf613ccc3addf
LukeNeon <Lukas.vikman@gmail.com>
2022-06-22 13:46:12 +0200

    header start

---
+++ src/header.js
@@ -0,0 +1,25 @@
+import "./header.css";
+
+const Header = () => {
+    return (
+        <div className="pog">
+            <div className="box1">
+                <button>Commers</button>
+            </div>
+            <div className="box2">
+                <button>Click hear to cum</button>
+            </div>
+            <div className="box3">
+                <button>Any Gammers? on god no cap</button>
+            </div>
+        </div>
+    );
+}
+
+
+
+
+
+
+
+export default Header
