a0bd7dfec499a6721b1be47ed8818cac9266afd5
LukeNeon <Lukas.vikman@gmail.com>
2022-06-27 09:41:07 +0200

    reset

--- src/header.js
+++ src/header.js
@@ -1,17 +1,7 @@
-import "./header.css";
-
 const Header = () => {
     return (
-        <div className="pog">
-            <div className="box1">
-                <button>Commers</button>
-            </div>
-            <div className="box2">
-                <button>Click hear to cum</button>
-            </div>
-            <div className="box3">
-                <button>Any Gammers? on god no cap</button>
-            </div>
+        <div>
+           
         </div>
     );
 }
